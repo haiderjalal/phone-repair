b1436049feaa0f09e0b2778755c9f4d9b02cb2b9
haiderjalal <haiderjalal1555@gmail.com>
2025-08-19 22:46:40 +0500

    image added in section

--- components/sections/home3/Skill.js
+++ components/sections/home3/Skill.js
@@ -60,7 +60,7 @@ export default function Skill() {
                             <div className="skill-one__right-img-box wow slideInRight" data-wow-delay="100ms"
                                 data-wow-duration="2500ms">
                                 <div className="skill-one__right-img">
-                                    <img src="assets/images/resources/skill-1-1.jpg" alt=""/>
+                                    <img src="assets/images/services/service-1.jpg" alt=""/>
                                     <div className="skill-one__video-link">
                                         <a onClick={() => setOpen(true)} className="video-popup">
                                             <div className="skill-one__video-icon">
