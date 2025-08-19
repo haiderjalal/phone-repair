c2d02497d46ad062bb3209a60e6c4edf9c9ea60a
haiderjalal <haiderjalal1555@gmail.com>
2025-08-19 19:31:19 +0500

    home banner updated

--- components/sections/home3/Banner.js
+++ components/sections/home3/Banner.js
@@ -37,7 +37,7 @@ export default function Banner() {
                 <SwiperSlide>
                 <div className="item">
                     <div className="main-slider-three__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider-3-1.jpg)' }} >
+                        style={{ backgroundImage: 'url(assets/images/backgrounds/services-hero1.jpg)' }} >
                     </div>
                     <div className="main-slider-three__shape-1">
                         <img src="assets/images/shapes/main-slider-three-shape-1.png" alt=""/>
@@ -51,7 +51,7 @@ export default function Banner() {
                     <div className="container">
                         <div className="main-slider-three__content">
                             <div className="main-slider-three__sub-title-box">
-                                <p className="main-slider-three__sub-title">Best Repairs</p>
+                                <p className="main-slider-three__sub-title">Based in NewCastle</p>
                             </div>
                             <h2 className="main-slider-three__title">We Are Experts At <br/> Cell Phone Repair!</h2>
                             <p className="main-slider-three__text">We have been operating for over a decade, providing
@@ -64,66 +64,7 @@ export default function Banner() {
                     </div>
                 </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                <div className="item">
-                    <div className="main-slider-three__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider-3-2.jpg)' }} >
-                    </div>
-                    <div className="main-slider-three__shape-1">
-                        <img src="assets/images/shapes/main-slider-three-shape-1.png" alt=""/>
-                    </div>
-                    <div className="main-slider-three__shape-2">
-                        <img src="assets/images/shapes/main-slider-three-shape-2.png" alt=""/>
-                    </div>
-                    <div className="main-slider-three__shape-3">
-                        <img src="assets/images/shapes/main-slider-three-shape-3.png" alt="" className="img-bounce" />
-                    </div>
-                    <div className="container">
-                        <div className="main-slider-three__content">
-                            <div className="main-slider-three__sub-title-box">
-                                <p className="main-slider-three__sub-title">Best Repairs</p>
-                            </div>
-                            <h2 className="main-slider-three__title">We Are Experts At <br/> Cell Phone Repair!</h2>
-                            <p className="main-slider-three__text">We have been operating for over a decade, providing
-                                top-notch services We have been <br/> operating for over a decade lenitive antique
-                                corrupt quo quad molestation</p>
-                            <div className="main-slider-three__btn-box">
-                                <Link href="services" className="main-slider-three__btn thm-btn">Repair My Device</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div className="item">
-                    <div className="main-slider-three__bg"
-                        style={{ backgroundImage: 'url(assets/images/backgrounds/slider-3-3.jpg)' }} >
-                    </div>
-                    <div className="main-slider-three__shape-1">
-                        <img src="assets/images/shapes/main-slider-three-shape-1.png" alt=""/>
-                    </div>
-                    <div className="main-slider-three__shape-2">
-                        <img src="assets/images/shapes/main-slider-three-shape-2.png" alt=""/>
-                    </div>
-                    <div className="main-slider-three__shape-3">
-                        <img src="assets/images/shapes/main-slider-three-shape-3.png" alt="" className="img-bounce" />
-                    </div>
-                    <div className="container">
-                        <div className="main-slider-three__content">
-                            <div className="main-slider-three__sub-title-box">
-                                <p className="main-slider-three__sub-title">Best Repairs</p>
-                            </div>
-                            <h2 className="main-slider-three__title">We Are Experts At <br/> Cell Phone Repair!</h2>
-                            <p className="main-slider-three__text">We have been operating for over a decade, providing
-                                top-notch services We have been <br/> operating for over a decade lenitive antique
-                                corrupt quo quad molestation</p>
-                            <div className="main-slider-three__btn-box">
-                                <Link href="services" className="main-slider-three__btn thm-btn">Repair My Device</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </SwiperSlide>
+     
             </Swiper>
         </section>
         {/*Main Slider Start */} 
